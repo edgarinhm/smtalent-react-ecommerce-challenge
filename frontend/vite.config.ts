ec43b78fbd511fe07ecf437d6a0e5d3211fbcc97
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-06 11:19:57 -0500

    add frontend vites fix css types for custom lib in vite config add app tests fix testing lib jest dom add left hook config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      include: ["src/components"],
+      exclude: ["**/*.stories.tsx"],
+    },
+    server: {
+      deps: {
+        inline: ["@poweredge/smtalent-challenge-lib"],
+      },
+    },
+  },
+});
