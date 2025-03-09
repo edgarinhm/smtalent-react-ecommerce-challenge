6360a9d53f5d57481d9c51f9a28d7bf1fa11448f
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-09 09:10:02 -0500

    add layout styles add card template

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import styles from "./Layout.module.css";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <main id={"page-content"} role="main">
+      <main id={"page-content"} role="main" className={styles.main}>
         <Outlet />
       </main>
       <Footer />
