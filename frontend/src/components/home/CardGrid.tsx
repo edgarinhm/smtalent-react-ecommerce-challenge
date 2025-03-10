722537679b2f6add276ca2cd133fa05faf542ac1
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-10 18:31:00 -0500

    add currency lib add primary color hover styles add cart footer button styles

--- frontend/src/components/home/CardGrid.tsx
+++ frontend/src/components/home/CardGrid.tsx
@@ -1,6 +1,8 @@
 import { Card } from "../../common/components/card/Card";
 import styles from "./CardGrid.module.css";
 import { ProductModel } from "../../common/models/product-model";
+import currency from "currency.js";
+import { Button } from "@poweredge/smtalent-challenge-lib";
 
 interface CardGridProps {
   products: ProductModel[];
@@ -13,6 +15,17 @@ export const CardGrid = ({ products }: CardGridProps) => {
         return (
           <Card key={product.id}>
             <Card.CardHeader title={product.name} />
+            <span>{currency(product.price).format()}</span>
+            <Card.CardFooter>
+              <Button
+                style={{ textTransform: "inherit" }}
+                variant="floating-action"
+                size="small"
+                className={`poweredge-color-primary ${styles["card-footer-action-buttons"]}`}
+              >
+                {"Add to cart"}
+              </Button>
+            </Card.CardFooter>
           </Card>
         );
       })}
